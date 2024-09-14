f08bf964e8f6de40473a4cf2063973a11e0077f1
Topi Santakivi <topi.santakivi@luotocompany.fi>
2024-09-14 22:22:04 +0300

    Add support for OpenRouter

--- actions/openai-actions.ts
+++ actions/openai-actions.ts
@@ -1,13 +1,22 @@
 "use server";
 
 import { Message } from "@/types/messages/messages-types";
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
 import { generateText } from "ai";
 
-export async function generateMessage(model: "o1-preview" | "o1-mini", messages: Message[]) {
+const baseURL = process.env.OPENAI_BASE_URL || "https://api.openai.com/v1";
+const openai = createOpenAI({
+  baseURL,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function generateMessage(
+  model: "o1-preview" | "o1-mini",
+  messages: Message[]
+) {
   const { text } = await generateText({
     model: openai(model),
-    messages
+    messages,
   });
 
   return text;
